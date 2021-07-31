ee6ed0a71af73c22aa9067d60090dc2aa5a74e28
Sylvain <sylvainlosey@gmail.com>
2021-07-31 17:09:29 +0200

    Add inkwell to cards

--- lib/src/widgets/eagle_card.dart
+++ lib/src/widgets/eagle_card.dart
@@ -5,18 +5,25 @@ const blueGray200 = Color(0xffe2e8f0);
 
 class EagleCard extends StatelessWidget {
   final Widget child;
+  void Function()? onTap;
   Color borderColor;
   EdgeInsetsGeometry padding;
 
   EagleCard(
       {required this.child,
+      this.onTap,
       this.padding = const EdgeInsets.all(16),
       this.borderColor = blueGray200});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Padding(child: child, padding: padding),
+        child: Padding(
+            child: InkWell(
+                child: child,
+                onTap: onTap,
+                borderRadius: BorderRadius.circular(8)),
+            padding: padding),
         color: Colors.white,
         elevation: 0,
         margin: EdgeInsets.zero,
