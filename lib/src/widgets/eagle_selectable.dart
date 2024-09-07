9de59b120832b927fa7d039139a7b95436a1f1d0
Sylvain Losey <sylvainlosey@gmail.com>
2024-09-07 12:04:57 +0200

    Improve Selectable Item

--- lib/src/widgets/eagle_selectable.dart
+++ lib/src/widgets/eagle_selectable.dart
@@ -5,17 +5,19 @@ import 'package:tailwind_colors/tailwind_colors.dart';
 
 class EagleSelectableItem extends StatefulWidget {
   const EagleSelectableItem({
-    Key? key,
+    super.key,
     this.selected = false,
     this.padding = const EdgeInsets.all(8),
     this.onPressed,
+    this.showIcons = true,
     required this.child,
-  }) : super(key: key);
+  });
 
   final VoidCallback? onPressed;
   final Widget child;
   final bool selected;
   final EdgeInsetsGeometry padding;
+  final bool showIcons;
 
   @override
   State<EagleSelectableItem> createState() => _EagleSelectableItemState();
@@ -87,14 +89,33 @@ class _EagleSelectableItemState extends State<EagleSelectableItem> {
             borderRadius: BorderRadius.circular(defaultRadius),
           ),
           child: Container(
-            padding: widget.padding,
-            decoration: BoxDecoration(
-              color: color,
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(defaultRadius - 2),
-            ),
-            child: Row(children: [Flexible(child: widget.child)]),
-          ),
+              padding: widget.padding,
+              decoration: BoxDecoration(
+                color: color,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(defaultRadius - 2),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Row(
+                      children: [
+                        if (widget.showIcons && !widget.selected) ...[
+                          const Icon(Icons.chevron_right,
+                              color: EagleColors.oustBlendMain),
+                          EagleSpacing.horiz2,
+                        ],
+                        Flexible(child: widget.child),
+                      ],
+                    ),
+                  ),
+                  if (widget.showIcons && widget.selected) ...[
+                    EagleSpacing.horiz2,
+                    Icon(Icons.close, color: TWTwoColors.blueGray.shade600),
+                  ]
+                ],
+              )),
         ),
       ),
     );
